25b512bf2db87c2859908cd37910cc4bf2b7aefc
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-17 00:01:06 +0530

    style(page): :lipstick: Add styling to home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
-      >
-        <Header />
+      <body className={josefin.className}>
+        <div className="bg-primary-950 text-primary-100 min-h-screen flex flex-col relative">
+          <Header />
 
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+          <div className="flex-1 px-8 py-12">
+            <main className="max-w-7xl mx-auto">{children}</main>
+          </div>
         </div>
       </body>
     </html>
