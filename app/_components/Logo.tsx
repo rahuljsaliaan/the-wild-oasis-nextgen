fa6824ec644217f290044b1375eb42d29ab1dcd6
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-16 22:00:27 +0530

    feat(_components): :sparkles: Add reusable UI components

--- app/_components/Logo.tsx
+++ app/_components/Logo.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
-export default function Logo() {
+function Logo() {
   return (
     <a href="/" className="flex items-center gap-4 z-10">
-      <Image src="/logo.png" alt="The Wild Oasis logo" height={60} width={60} />
+      <Image height={60} width={60} alt="The Wild Oasis logo" src="/logo.png" />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
     </a>
   );
 }
+
+export default Logo;
