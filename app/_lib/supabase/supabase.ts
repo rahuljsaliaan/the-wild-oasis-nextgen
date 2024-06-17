a814ef61bc00d179b4c7c1d4db924907ccb18a4c
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-18 00:33:28 +0530

    fix(supabase, SelectCountry): :bug: Config and supabase imports

--- app/_lib/supabase/supabase.ts
+++ app/_lib/supabase/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import config from "@/app/_lib/config";
+import config from "@/app/_lib/config/config";
 import { Database } from "./database.types";
 
 const supabase = createClient<Database>(
