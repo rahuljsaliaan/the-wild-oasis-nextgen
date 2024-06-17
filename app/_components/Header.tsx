b98502c882f5e98496000d45f853cb5c1dadb8e4
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-17 15:08:39 +0530

    style(Header): :lipstick: Correct header styling on home page

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "@/app/_components/Logo";
 
 function Header() {
   return (
-    <header className="border-b border-primary-900 px-8 py-5 sticky top-0 bg-primary-950/50 backdrop-blur-lg">
+    <header className="px-8 py-5 sticky top-0 backdrop-blur-lg z-50">
       <div className="flex justify-between items-center max-w-7xl mx-auto">
         <Logo />
         <Navigation />
