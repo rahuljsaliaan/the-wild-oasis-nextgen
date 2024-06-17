5d670d02a7e6a31c30f9b8c6bab8ca386095952f
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-18 00:19:59 +0530

    feat(supabase): :sparkles: Add supabase client with types

---
+++ app/_lib/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  env: {
+    supabaseUrl: process.env.SUPABASE_URL!,
+    supabaseKey: process.env.SUPABASE_KEY!,
+  },
+};
+
+export default config;
---
+++ app/_lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+import config from "@/app/_lib/config";
+import { Database } from "./database.types";
+
+const supabase = createClient<Database>(
+  config.env.supabaseUrl,
+  config.env.supabaseKey
+);
+
+export default supabase;
