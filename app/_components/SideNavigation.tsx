2e27d5440d47823b95a89eff2e0adad35136da6f
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-07-07 22:45:10 +0530

    feat(SideNavigation): :sparkles: Add navigation highlighting on currently selected route

--- app/_components/SideNavigation.tsx
+++ app/_components/SideNavigation.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import {
   CalendarDaysIcon,
   HomeIcon,
   UserIcon,
 } from "@heroicons/react/24/solid";
 import SignOutButton from "@/app/_components/SignOutButton";
+import { usePathname } from "next/navigation";
 
 const navLinks = [
   {
@@ -24,13 +27,17 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <a
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathname === link.href ? "bg-primary-900" : ""
+              }`}
               href={link.href}
             >
               {link.icon}
